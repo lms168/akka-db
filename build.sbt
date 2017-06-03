298a4ff3171181941183bb42c067e57a30569d9f
zhangdi <zhangdi@chinamobiad.com>
2017-06-03 09:08:14 +0800

    (添加) 整理结构

--- build.sbt
+++ build.sbt
@@ -1,33 +1,37 @@
-
+//System sbt config
+import NativePackagerHelper._
 
 enablePlugins(JavaAppPackaging)
 enablePlugins(UniversalPlugin)
 
-lazy val root = (project in file(".")).
-  settings(
-    inThisBuild(List(
-      organization := "com.example",
-      scalaVersion := "2.12.1",
-      version      := "0.1.0"
-    )),
-    name := "sbt-template",
-    libraryDependencies += "com.typesafe.slick" %% "slick" % "3.2.0",
-    libraryDependencies += "org.slf4j" % "slf4j-nop" % "1.6.4",
-    libraryDependencies += "com.typesafe.slick" %% "slick-hikaricp" % "3.2.0",
-    libraryDependencies += "mysql" % "mysql-connector-java" % "5.1.18",
-    libraryDependencies += "com.typesafe" % "config" % "1.2.1"
-
-  )
-
-import NativePackagerHelper._
 mappings in Universal += file("src/main/config/app.sh") -> "app.sh"
 mappings in Universal ++= {
-    ((sourceDirectory in Compile).value / s"config/${System.getProperty("conf.path")}" * "*").get.map { f => 
-      f -> s"config/${f.name}"
-    }
+  ((sourceDirectory in Compile).value / s"config/${System.getProperty("conf")}" * "*").get.map { f =>
+    f -> s"config/${f.name}"
+  }
+}
+addCommandAlias("pkg", ";universal:packageZipTarball")
+
+//Config - Root
+lazy val root = (project in file(".")).settings(name := "adx-etl", commonSettings, librarySettings)
+
+lazy val commonSettings = Seq(
+  organization := "com.zzcm.adx",
+  scalaVersion := "2.11.8",
+  version      := "0.1.0"
+)
+
+lazy val librarySettings = {
+  libraryDependencies ++= Seq(
+    "com.typesafe.slick" %% "slick" % "3.2.0",
+    "org.slf4j" % "slf4j-nop" % "1.6.4",
+    "com.typesafe.slick" %% "slick-hikaricp" % "3.2.0",
+    "mysql" % "mysql-connector-java" % "5.1.18",
+    "com.typesafe" % "config" % "1.2.1"
+  )
 }
 
-addCommandAlias("install", ";universal:packageZipTarball")
+
 
 
 
--- src/main/scala/com/zzcm/App.scala
+++ src/main/scala/com/zzcm/App.scala
@@ -1,69 +1,9 @@
 package com.zzcm
 
-import com.typesafe.config._
+import example.Hello
 
-import scala.io.Source
-import scala.concurrent.Await
-import scala.concurrent.duration._
+object App extends App {
 
-import slick.driver.MySQLDriver.api._
+  Hello.main(args)
 
-import example.bean.KeyValue
-
-object App extends Greeting with App {
-  println(greeting)
-
-  //read file 1M
-  val source = Source.fromFile("/home/zhangdi/NetDisk/delete/z20170523/szdsp-kv/split/D1.txt","UTF-8") 
-  
-  val lineIterator = source.getLines
-  
-  /*
-  val length = 90
-  for (l <- lineIterator) {
-    if (l.length > length) println(l.substring(length)) else println(l)
-  }
-  */
-
-  val subLength = 90
-  val rows = lineIterator.filter(_.length >subLength).map(_.substring(subLength))
-
-  val kvs = rows.map(_.split(" ")).filter(_.length == 3).map(_.dropRight(1)).map(x => ((x(0).split(":")(1) + ":" + x(0).split(":")(2),x(1).split(":")(1) ))).map(x => new KeyValue(x._1, x._2.toLong))
-
-  val config = ConfigFactory.load()
-  //DataBase Config
-  val db = Database.forURL(
-    config.getString("db.conf.url"), //"jdbc:mysql://192.168.0.223:3306/iadbigdata",
-    config.getString("db.conf.user"), //"zzmanager",
-    config.getString("db.conf.pwd"), //"iadMOB-2013@0622)",
-    driver= config.getString("db.conf.driver") //"com.mysql.jdbc.Driver"
-  )
-
-  //Insert Data Mysql
-  try{
-  
-     val obj= db.run(sql"select count(id) from UNStatesMsg_PV_MIN".as[Int].head)
-     val result = Await.result(obj, 2.seconds)
-     println(result)
-
-
-    val num = kvs.map( x => {
-      val param = x.reset
-      println(param)
-      val insertRow = sql"INSERT INTO `UNStatesMsg_PV_MIN` (`appId`, `date`, `hour`, `minute`, `dimension_one_key`, `dimension_one_value`, `operator`, `value`) VALUES (#${param("appId")}, #${param("date")}, #${param("hour")}, '20', 'ad', '459', 'QUERY', '1')".as[Int]
-      Await.result(db.run(insertRow), 2.seconds)
-    }).size
-
-
-
-  }catch{
-    case e: Exception => println(e)
-  }
-
-  println("... End ...")
-
-}
-
-trait Greeting {
-  lazy val greeting: String = "hello"
 }
