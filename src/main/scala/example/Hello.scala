f9509b46bd287036374089fa55a708cfc7286ff5
zhangdi <zhangdi@chinamobiad.com>
2017-06-03 09:13:00 +0800

    (添加) 读取配置文件

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,7 +1,18 @@
 package example
 
+import com.typesafe.config.ConfigFactory
+
 object Hello extends Greeting with App {
+
   println(greeting)
+
+  //Load Config
+  val config= ConfigFactory.load()
+  val httpConfig = config.getConfig("main.http")
+  val host = httpConfig.getString("host")
+  val port = httpConfig.getInt("port")
+  println(s"$host:$port")
+
 }
 
 trait Greeting {
